5107a490787b405f6acd3c286968cd93e3d046cd
William <williamhess@gmail.com>
2023-10-27 11:36:28 -0700

    Add game logic

--- script.js
+++ script.js
@@ -1,4 +1,142 @@
-// You’re going to store the gameboard as an array inside of a Gameboard object, so start there!
+// Gameboard module: An IIFE which returns { isCellEmpty, markCell, getBoard }
+const Gameboard = (function () {
+      // Initialize board array
+  const board = ["", "", "", "", "", "", "", "", ""];
+  const gameStatus = "playing";
+
+      // Get cell is empty
+      function isCellEmpty(index) {
+        return board[index] === "";
+      }
+
+      // Set cell marker
+      function markCell(index, marker) {
+        if (isCellEmpty(index)) {
+          board[index] = marker;
+          return true;
+        }
+        return false;
+      }
+
+      // Return board array
+      function getBoard() {
+        return board;
+      }
+      
+      // Reset Gameboard
+      function resetBoard() {
+        board = ["", "", "", "", "", "", "", "", ""];
+        gameStatus = "playing";
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell) => {
+          cell.addEventListener('click', () => cell.textContent = "");
+            });
+        GameController.updateGameDisplay("PlayerX's turn");
+      }
+  
+      // Expose all the functions defined in the module
+      return { isCellEmpty, markCell, getBoard, resetBoard };
+    })();
+
+  // Player factory function)
+  const Player = (name, marker) => {
+      return { name, marker };
+    };
+
+  const playerX = Player("Player X", "X");
+  const playerO = Player("Player O", "O");
+
+  // Game controller module
+  const GameController = (function () {
+      
+    // Player X starts game
+    let currentPlayer = playerX;
+    let displayText = "";
+
+    // Switch current Player
+    function switchPlayer() {
+      currentPlayer = currentPlayer === playerX ? playerO : playerX;
+      updateGameDisplay(currentPlayer);
+    }
+  
+    // Check for three in a row
+    function areThreeInARow() {
+// 012	if (arr[0] === arr[1] && arr[0] === arr[2])
+// 345
+// 678
+
+// 036
+// 147
+// 258
+
+// 048
+// 246
+      if (Gameboard.board) {
+        return true;
+      } else {
+        return false;
+      }
+
+    // Set game Display
+    function updateGameDisplay(gameInfo) {
+      const gameDisplay = document.querySelector('.gameDisplay');
+      gameDisplay.textContent = gameInfo;
+    }
+
+    // When any cell is clicked, do this
+    function handleCellClick(event) {
+      // Target the clicked cell
+      const cell = event.target;
+      // Get correct board array index
+      const index = cell.dataset.index; // parse int?
+    
+      //  If cell is empty and gameStatus == "playing"
+      if (Gameboard.isCellEmpty(index) && Gameboard.gameStatus === "playing") {
+        // Change inner text to currentPlayerSymbol    
+        cell.textContent = currentPlayer.marker;
+        Gameboard.markCell(index, currentPlayer.marker);
+
+        if (areThreeInARow()) {
+          Gameboard.gameStatus = `${currentPlayer} wins!`;
+          displayText = Gameboard.gameStatus;
+          updateGameDisplay(displayText);
+        } // Else if boardArray contains isEmpty
+        else if (Gameboard.board.includes("")) {
+          switchPlayer();
+        } // Else change gameStatus to It’s a tie
+        else {
+          Gameboard.gameStatus === `It’s a tie`;
+          displayText = Gameboard.gameStatus
+          updateGameDisplay(displayText);
+        }
+      }
+    }
+
+    function addEventListeners() {
+      const resetButton = document.querySelector('.resetButton');
+      resetButton.addEventListener('click', Gameboard.resetBoard)
+
+      const cells = document.querySelectorAll('.cell');
+      cells.forEach((cell) => {
+        cell.addEventListener('click', handleCellClick);
+      });
+    }
+      return { addEventListeners, updateGameDisplay };
+  })();
+
+GameController.addEventListeners();
+GameController.updateGameDisplay();
+
+
+// Check current player
+//  if button’s inner text is empty
+// Change inner text to currentPlayerSymbol
+
+// if three in a row
+// Change gameStatusButton to currentPlayer wins!
+// Else if boardArray contains isEmpty
+// Toggle current player
+// Else change gameStatusButton to It’s a tie
 
 // const GameBoard = {};
 // const isEmpty = true;
@@ -55,6 +193,7 @@
 //     }
 // }
 
+// You’re going to store the gameboard as an array inside of a Gameboard object, so start there!
 // Your players are also going to be stored in objects, and you’re probably going to want an object to control the flow of the game itself.
 // Your main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory.
 // Rule of thumb: 
