3d8a4e44fe4ae2cbe75a775f74e5c169da070247
William <williamhess@gmail.com>
2023-11-02 05:44:51 -0700

    Prevent crash when Enter key pressed

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Clean up the interface to allow players to put in their names, include a button to start/restart
 
 // Optional - If you’re feeling ambitious create an AI so that a player can play against the computer!
 // Start by just getting the computer to make a random legal move.
@@ -6,11 +5,9 @@
 
 const Gameboard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
-
   function isCellEmpty(index) {
     return board[index] === "";
   }
-
   function markCell(index, marker) {
     if (isCellEmpty(index)) {
       board[index] = marker;
@@ -18,15 +15,12 @@ const Gameboard = (() => {
     }
     return false;
   }
-
   function getBoard() {
     return board;
   }
-
   function setBoard(updatedBoard) {
     board = updatedBoard;
   }
-
   return { isCellEmpty, markCell, getBoard, setBoard };
 })();
 
@@ -44,26 +38,22 @@ const GameController = (() => {
 
   function switchPlayer() {
     currentPlayer = currentPlayer === playerX ? playerO : playerX;
-
     if (currentPlayer === playerO) {
       updateGameDisplay(`${playerName}'s Opponent's Turn`);
     } else {
       updateGameDisplay(`${playerName}'s Turn`);
     }
   }
-
   function updateGameDisplay(gameInfo) {
     const gameDisplay = document.querySelector('.gameDisplay');
     gameDisplay.textContent = gameInfo;
   }
-
   function areThreeInARow(board) {
     const winningCombos = [
       [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
       [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
       [0, 4, 8], [2, 4, 6] // Diagonals
     ];
-
     for (const combo of winningCombos) {
       const [a, b, c] = combo;
       if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
@@ -72,19 +62,15 @@ const GameController = (() => {
     }
     return false;
   }
-
   function handleCellClick(event) {
     const sqr = event.target;
     const stringIndex = sqr.getAttribute('data-index');
     const index = parseInt(stringIndex, 10);
-
     if (Gameboard.isCellEmpty(index) && gameStatus === "playing") {
       sqr.textContent = currentPlayer.marker;
       Gameboard.markCell(index, currentPlayer.marker);
-
       if (areThreeInARow(Gameboard.getBoard())) {
         gameStatus = "!playing";
-
         if (currentPlayer === playerO) {
           updateGameDisplay(`${playerName}'s Opponent wins!`);
         } else {
@@ -98,47 +84,27 @@ const GameController = (() => {
       }
     }
   }
-
   function resetBoard() {
     Gameboard.setBoard(["", "", "", "", "", "", "", "", ""]);
     gameStatus = "playing";
     currentPlayer = playerX;
-    // currentPlayer = playerX;
     const squares = document.querySelectorAll('.square');
     squares.forEach((sqr) => {
       sqr.textContent = "";
     });
-
-    // if (playerName === "X")
-    // updateGameDisplay(`${currentPlayer.name}'s Turn`);
-    // else {
-    //   updateGameDisplay(`${playerName}'s Turn`);
-    // }
-    // updateGameDisplay(`Enter new name or play as ${playerName}`);
-    updateGameDisplay(`You may start or enter a new name`);
-
-// if a new name is entered, it would be nice to update the display!!
-
+    updateGameDisplay(`Start Game / Change Name`);
   }
-
   function submitFormInputs(event) { //
-    // Get the name of the user and their choice.
+    // prevent form submission
     event.preventDefault();
-
     const name = document.querySelector('input[name="name"]').value;
     playerName = name;
-
     if (playerName === "")
       updateGameDisplay("ENTER YOUR NAME");
     else if (currentPlayer !== playerO) {
-          // only if  the game isn't displaying the other guy's turn should the updateGameDisplay () be called
       updateGameDisplay(`${playerName}'s Turn`);
       gameStatus = "playing";
     }
-
-    console.log(name);
-    console.log(playerName);
-
     const selectedOpponent = document.querySelector('input[name="selectedOpponent"]:checked');
     if (selectedOpponent) {
       // The radio button is checked.
@@ -151,40 +117,32 @@ const GameController = (() => {
       // Start the new version of game. Synthetic pics random empty cell.
       console.log(selectedOpponent.value);
 
-    
-
+  
     } else if (selectedOpponent.value === 'organic') {
             // Start the default game
             console.log(selectedOpponent.value);
 
     }
   }
-
   function addEventListeners() {
     const cells = document.querySelectorAll(".square");
     cells.forEach((cell) => {
       cell.addEventListener('click', handleCellClick);
     });
-
     const resetButton = document.querySelector('.resetButton');
     resetButton.addEventListener('click', resetBoard);
-
     const submitButton = document.querySelector('#submitButton');
     submitButton.addEventListener('click', submitFormInputs);
-  }
+    const keyboardInput = document.querySelector("#name");
 
+    // enter key has limited effect as written
+    keyboardInput.addEventListener("keypress", function(event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        submitButton.click();
+      }
+    });
+  }
   return { addEventListeners };
 })();
-
 GameController.addEventListeners();
-
-// Rule of thumb: 
-// If you only ever need ONE of something(gameBoard, displayController), use a module.
-
-// If you need multiples of something(players!), create them with factories.
-
-// Think carefully about where each bit of logic should reside. Each little piece of functionality should be able to fit in the GAME, PLAYER, or GAMEBOARD objects.
-
-//Take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later!
-
-// If you’re having trouble, Building a house from the inside out is a great article that lays out a highly applicable example of how you might organize your code for this project.
